f83afed59045264bffdf92a2545e4ad21196e026
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2022-04-09 13:12:03 +0200

    Refactor WebCrawler - split long method in submethods

--- src/main/java/app/domain/WebCrawler.java
+++ src/main/java/app/domain/WebCrawler.java
@@ -24,19 +24,33 @@ public class WebCrawler {
   }
 
   Report crawlUrl(URL url, int depth, Set<URL> visitedUrls) {
+    Page page = loadPage(url, visitedUrls);
+    Report resultReport = translatePage(page);
+    if (depth <= 1) {
+      return resultReport;
+    }
+    return crawlSubPages(page, resultReport, depth - 1, visitedUrls);
+  }
+
+  Page loadPage(URL url, Set<URL> visitedUrls) {
     visitedUrls.add(url);
     Page page = pageLoader.loadPage(url);
+    return page;
+  }
+
+  Report translatePage(Page page) {
     Report resultReport = new Report();
     resultReport.addPage(page);
     Locale targetLanguage = inputParameters.getTargetLanguage();
     resultReport = translationService.translateReport(resultReport, targetLanguage);
-    if (depth <= 1) {
-      return resultReport;
-    }
+    return resultReport;
+  }
+
+  Report crawlSubPages(Page page, Report resultReport, int subDepth, Set<URL> visitedUrls) {
     return page.streamLinks()
             .map(Link::url)
             .filter(u -> !visitedUrls.contains(u))
-            .map(u -> crawlUrl(u, depth - 1, visitedUrls))
+            .map(u -> crawlUrl(u, subDepth, visitedUrls))
             .reduce(resultReport, Report::merge);
   }
 }

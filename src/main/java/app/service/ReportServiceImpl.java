2f7faa581495711fa19775154a2753d6d77069fd
Nermin Topalovic <nerminto@edu.aau.at>
2022-04-11 14:46:41 +0200

    Refactored ReportServiceImpl class by splitting the long createMardownReport() method into several smaller methods

--- src/main/java/app/service/ReportServiceImpl.java
+++ src/main/java/app/service/ReportServiceImpl.java
@@ -15,62 +15,73 @@ import java.util.stream.Stream;
 
 public class ReportServiceImpl implements ReportService {
 
-  private final String br = "<br>";
+  private final String markDownBreak = "<br>";
 
     @Override
     public void createMarkdownReport(Report targetReport, CommandLine commandLineInput) {
+
+      String reportString = "";
+      reportString += createMetaInformationAsString(commandLineInput);
+      reportString += createPageContentAsString(targetReport.getPageList());
+
       try{
         FileWriter fileWriter = new FileWriter(transformURLintoName(commandLineInput.getUrl()) + ".md");
-        //TODO: Write Meta information
-        fileWriter.write("Input:\n");
-        fileWriter.write(br + "<"+ commandLineInput.getUrl() +">\n");
-        fileWriter.write(br + "depth: " + commandLineInput.getDepth() + "\n");
-        fileWriter.write(br + "target language: " + commandLineInput.getTargetLanguage() + "\n");
-        fileWriter.write(br + "report:\n");
-
-        List<Page> pageList = targetReport.getPageList();
-
-
-        for(Page page: pageList){
-
-          StringBuilder urlLineToWrite = new StringBuilder();
-
-          List<Heading> headingList = page.getHeadings();
-          headingList.sort(new Comparator<Heading>() {
-            @Override
-            public int compare(Heading o1, Heading o2) {
-              return o1.getHeadingDepth() - o2.getHeadingDepth();
-            }
-          });
-
-          for(Heading heading : headingList){
-            for(int i = 0; i< heading.getHeadingDepth();i++){
-              urlLineToWrite.append("#");
-            }
-            urlLineToWrite.append(" "+heading.getHeadingTitle() +"\n");
-          }
-
-          urlLineToWrite.append(br+"--> ");
-
-          page.streamLinks()
-                  .forEach(l->
-                          urlLineToWrite.append(br+"-->"+
-                                  ((l.broken())?"broken link ":"link to <") +
-                                  l.url() + ">\n"));
-          urlLineToWrite.append(br+ "\n");
-          fileWriter.write(urlLineToWrite.toString());
-        }
-
+        fileWriter.write(reportString);
         fileWriter.close();
-
       }catch (IOException ioException){
         System.out.println("An error occurred during file writing.\n");
         ioException.printStackTrace();
       }
     }
 
+    private String createMetaInformationAsString(CommandLine commandLineInput){
+      return "Input:\n"
+              + markDownBreak + "<"+ commandLineInput.getUrl() +">\n"
+              + markDownBreak + "depth: " + commandLineInput.getDepth() + "\n"
+              + markDownBreak + "target language: " + commandLineInput.getTargetLanguage() + "\n"
+              + markDownBreak + "report:\n";
+    }
+
+    private String createPageContentAsString(List<Page> pageList){
+      StringBuilder pageContent = new StringBuilder();
+
+      for(Page page: pageList){
+       pageContent.append(createHeadingsAsString(page.getHeadings()));
+       pageContent.append(createLinksAsString(page.streamLinks()));
+      }
+      return  pageContent.toString();
+    }
+
+    private String createHeadingsAsString(List<Heading> headingList){
+      headingList.sort((o1, o2) -> o1.getHeadingDepth() - o2.getHeadingDepth());
+
+      StringBuilder headingString = new StringBuilder();
+      for(Heading heading : headingList){
+        for(int i = 0; i< heading.getHeadingDepth();i++){
+          headingString.append('#');
+        }
+        headingString.append(" ");
+        headingString.append(heading.getHeadingTitle());
+        headingString.append("\n");
+      }
+      return headingString.toString();
+    }
+
+    private String createLinksAsString(Stream<Link> linkStream){
+      StringBuilder linksAsString = new StringBuilder();
+
+      linkStream.forEach(link-> linksAsString.append(createSingleLinkAsString(link)));
+      linksAsString.append(markDownBreak + "\n");
+
+      return linksAsString.toString();
+    }
+
+    private String createSingleLinkAsString(Link link){
+      return (markDownBreak + "-->"+ ((link.broken())?"broken link ":"link to <") + link.url() + ">\n");
+    }
+
     private String transformURLintoName(URL targetURL){
       // TODO transform URL into String
-      return targetURL.toString();
+      return "stub";
     }
 }
